8bfe1be5a3e38abb0adbd9a97c533ecf521d3fb6
Tiku-Kouki <k022g0079@g.neec.ac.jp>
2023-10-04 17:18:56 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -1,5 +1,30 @@
 #include<stdio.h>
+template <typename Type>
+
+Type Min(Type a, Type b) {
+	if (a < b) {
+		return a ;
+	}
+	return b;
+}
+
+const char* Min(char a, char b) {
+
+	const char* mes = "”ŽšˆÈŠO‘ã“ü‚Å‚«‚Ü‚¹‚ñ";
+	
+	return mes;
+	
+}
+
+
 
 int main(){
+
+	printf("%d\n", Min<int>(114, 514));
+	printf("%f\n", Min<float>(114, 514));
+	printf("%lf\n", Min<double>(614, 514));
+	printf("%s\n", Min('a', 'b'));
+
+
 
 }
