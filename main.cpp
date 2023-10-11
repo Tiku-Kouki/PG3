c5e9277eff1f228d3d0f349e1ee6a8d78318783b
Tiku-Kouki <k022g0079@g.neec.ac.jp>
2023-10-11 18:55:50 +0900

    01_02確認課題

--- main.cpp
+++ main.cpp
@@ -1,24 +1,56 @@
 #include<stdio.h>
 template <typename Type>
 
-Type Min(Type a, Type b) {
-	if (a < b) {
-		return a ;
+Type Max(Type a, Type b) {
+	if (a > b) {
+		return a;
 	}
 	return b;
 }
 
-const char* Min(char a, char b) {
+const char* Max(char a, char b) {
 
 	const char* mes = "数字以外代入できません";
-	
+
 	return mes;
-	
+
 }
 
-int main(){
-	printf("%d\n", Min<int>(114, 514));
-	printf("%f\n", Min<float>(114, 514));
-	printf("%lf\n", Min<double>(614, 514));
-	printf("%s\n", Min('a', 'b'));
+int Recursive(int n, int &num) {
+	
+	
+	if (n <= 1) {
+		return (num +=100);
+	}
+	int result = ((Recursive(n - 1, num) * 2) - 50);
+	num +=result;
+
+
+	if (n >= 4) {
+
+		return num;
+
+	}
+
+
+	return result;
+}
+
+
+int main() {
+
+	int n = 4;
+	int num = 0;
+	int result;
+	int Mn = 1072;
+	int result2;
+	result2 = Mn * n;
+
+	result = Recursive(n, num);
+
+	printf("再帰的な資金体系:%d 一般的な資金体系:%d\n", result, result2);
+
+	printf("%d\n", Max<int>(result, result2));
+
+	//printf("%s\n", Min('a', 'b'));
 }
