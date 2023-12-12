3b96d152a9556f9a283d17544a1fe34bcefc670b
Tiku-Kouki <k022g0079@g.neec.ac.jp>
2023-12-12 12:51:40 +0900

    05_01

--- main.cpp
+++ main.cpp
@@ -1,28 +1,69 @@
-#include<stdio.h>
-#include "Circle.h"
-#include "Rectangle.h"
+#include <stdio.h>
+#include <list>
+#include <iostream>
 
+using namespace std;
 
 
 
 int main() {
 	
 	
-	IShape* shape[2];
 
-	shape[0] = new Circle;
-	shape[1] = new Rectangle;
+	list<const char*> lst { 
+	"Tokyo","Kanda","Akihabara","Okachimachi",
+	"Uguisudani","Nippori","Tabata","Komagome",
+	"Sugame","Otsuka","Ikebukuro","Mejiro",
+	"Takabanobaba","Shin-Okubo","Shinjuku",
+	"Yoyogi","Harajuku","Shibuya",
+	"Ebisu","Meguro","Gotanda",
+	"Osaki","Shinagawa","Tamachi",
+	"Hamamatsucho","shimbashi","Yurakucho"
+
+	};
+
 	
 
-	shape[0]->size();
-	shape[1]->size();
+	printf("º˜a6”N‚Ì˜Hü\n");
+		
+	for (list<const char*>::iterator it_f = lst.begin(); it_f != lst.end(); ++it_f) {
+			
+			cout << *it_f << endl;
+	}
+	printf("\n");
+	printf("º˜a47”N‚Ì˜Hü\n");
 
-	shape[0]->draw();
-	shape[1]->draw();
+	for (list<const char*>::iterator it_f = lst.begin(); it_f != lst.end(); ++it_f) {
 
-	
-	for (int i = 0; i < 2; i++) {
-		delete shape[i];
+		if (*it_f == "Tabata") {
+
+			it_f = lst.insert(it_f, "Nishi-Nippori");
+
+			++it_f;
+		}
+		
+	}
+
+	for (list<const char*>::iterator it_f = lst.begin(); it_f != lst.end(); ++it_f) {
+
+		cout << *it_f << endl;
+	}
+	printf("\n");
+	printf("—ß˜a5”N‚Ì˜Hü\n");
+	for (list<const char*>::iterator it_f = lst.begin(); it_f != lst.end(); ++it_f) {
+
+		if (*it_f == "Tamachi") {
+
+			it_f = lst.insert(it_f, "Takanawa Gateway");
+
+			++it_f;
+		}
+
+	}
+
+	for (list<const char*>::iterator it_f = lst.begin(); it_f != lst.end(); ++it_f) {
+
+		cout << *it_f << endl;
 	}
 
 	return 0;
